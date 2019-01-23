f5bb2eaea6b3ebb7bca4734531b2a9675c6a2668
Juan Pablo Lopez <juanpablo@findasense.com>
2019-01-23 23:16:10 +0100

    trying url params

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 
-app.get('/', (req, res) => {
-    res.send('This a basic server for portfolio')
+// app.get('/', (req, res) => {
+//     res.send('This a basic server for portfolio')
+// })
+
+app.use((req, res, next) => {
+    console.log(`Time: ${Date.now()}`)
+    next()
+})
+
+app.use('/user/:id', (req, res, next) => {
+    console.log(`Request Type: ${req.method}`)
+    next()
+})
+
+app.use('/user/:id', (req, res, next) => {
+    res.send(`Hi you are user number: ${req.params.id}`)
 })
 
 app.listen(3000, () => {
