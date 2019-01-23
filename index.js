f548bd575b48ecf76504e748e6aaf4f026d09945
Juan Pablo Lopez <juanpablo@findasense.com>
2019-01-23 23:59:33 +0100

    using pug template engine

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express')
 const app = express()
-const router = express.Router()
 
-// app.get('/', (req, res) => {
-//     res.send('This a basic server for portfolio')
-// })
+// Sets Pug as template engine
+app.set('view engine', 'pug')
 
+// app.use is for biding middleware
 app.use((req, res, next) => {
-    console.log(`Time: ${Date.now()}`)
-    next()
-})
-
-app.use('/user/:id', (req, res, next) => {
+    let  current_datetime = new Date(Date.now())
+    console.log(`Date: ${current_datetime.getUTCDate()}/${current_datetime.getUTCMonth()+1}/${current_datetime.getUTCFullYear()}`)
     console.log(`Request Type: ${req.method}`)
     next()
 })
 
-app.use('/user/:id', (req, res, next) => {
+// this sets index route with index.html template
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Hello World',
+        message: 'This is my first express app',
+        paragraph: 'I think i gonna like this'
+    })
+})
+
+app.get('/user/:id', (req, res, next) => {
     res.send(`Hi you are user number: ${req.params.id}`)
 })
+
+
 
 app.listen(3000, () => {
     console.log('The server is running through port 3000')
